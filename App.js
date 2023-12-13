41206d218b2cf836f9b8f320e31c6f277009bab2
Muralidharan <132540445+murali0723@users.noreply.github.com>
2023-12-13 19:48:37 +0530

    Add files via upload
    /src
        /components
          TodoForm.js
          TodoList.js
          TodoItem.js
        App.js
        index.js
      package.json
      README.md
    folder model

---
+++ App.js
@@ -0,0 +1,34 @@
+ // src/App.js
+import React, { useState } from 'react';
+import TodoForm from './component/TodoForm';
+import TodoList from './component/TodoList';
+
+function App() {
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Date.now(), text, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  };
+
+  const deleteTodo = (id) => {
+    setTodos((prevTodos) => prevTodos.filter((todo) => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <h1>Todo App</h1>
+      <TodoForm addTodo={addTodo} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </div>
+  );
+}
+
+export default App;
---
+++ TodoForm.js
@@ -0,0 +1,28 @@
+// src/components/TodoForm.js
+import React, { useState } from 'react';
+
+const TodoForm = ({ addTodo }) => {
+  const [input, setInput] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (input.trim()) {
+      addTodo(input);
+      setInput('');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Add a new todo"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <button type="submit">Add Todo</button>
+    </form>
+  );
+};
+
+export default TodoForm;
